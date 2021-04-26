960cdeb57e1529f73feeb606fc62332e56150b37
Buck4437 <Buck4437@users.noreply.github.com>
2021-04-26 19:52:37 +0800

    done

---
+++ javascripts/database.js
@@ -0,0 +1,71 @@
+const DATA = [
+    {
+        name: "Main Project",
+        projects: [
+            {
+                name: "Oscillight",
+                desc: "A game about generating lights. Made for IGJ 2021.",
+                src: [
+                    {
+                        name: "Github",
+                        src: "https://buck4437.github.io/oscillight/"
+                    },
+                    {
+                        name: "Itch",
+                        src: "https://buck4437.itch.io/oscillight/"
+                    }
+                ]
+            },
+            {
+                name: "AD Save bank",
+                desc: "A place to store save files of the game 'Antimatter Dimensions'.",
+                src: "https://buck4437.github.io/save-bank/"
+            }
+        ]
+    },
+    {
+        name: "Old Project",
+        projects: [
+            {
+                name: "Spatial Layering",
+                desc: "My first game",
+                src: "https://buck4437.github.io/spatial-layering/"
+            },
+            {
+                name: "Color game",
+                desc: "My second game",
+                src: "https://buck4437.github.io/color-game/"
+            },
+            {
+                name: "Color idle",
+                desc: "My third game that is supposed to be a remake of color game",
+                src: "https://buck4437.github.io/color-idle/"
+            }
+        ]
+    },
+    {
+        name: "Jokes",
+        projects: [
+            {
+                name: "ORANGe IDLe",
+                desc: "Best GAMe ever",
+                src: "https://buck4437.github.io/orange-idle/"
+            },
+            {
+                name: "Paperclip Dimensions",
+                desc: "Antimatter Dimensions, but paperclips",
+                src: "https://buck4437.github.io/paperclip-dimensions/"
+            },
+        ]
+    },
+    // {
+    //     name: "Misc",
+    //     projects: [
+    //         {
+    //             name: "Puzzles",
+    //             desc: "This is where I'll dump all my puzzle ideas",
+    //             src: "puzzles/index.html"
+    //         }
+    //     ]
+    // }
+];
